bc619616d181e15a9832e7fbcdd5a5dacd37581e
Manuel Bernardelli <manuelbernardelli15@gmail.com>
2026-03-12 18:55:32 +0100

    Updated Directory for Java

---
+++ Java Autodidatta/Introduzione_java/src/Introduzione.java
@@ -0,0 +1,16 @@
+public class Introduzione {
+    public static void main(String[] args) {    //Ogni codice con Java deve avere questa linea per poter funzionare
+        System.out.print("Hello World");    //System.out.print permette di stampare a schermo quello che inseriamo all'interno dei doppi apici
+                                                // ogni comando in Java deve essere chiuso con ; come in C
+     
+        System.out.println("This is my first Java program");    //aggiungendo ln a print permette di mandare a capo dopo la stringa
+        System.out.println("This string is on another line!\n");          //altrimenti possiamo usare \n per mandare a capo
+        System.out.print("This one too!");
+
+        //I commenti come abbiamo visto sono aperti con //
+        /*
+        Dei commenti su più righe si aprono con (/*) */
+        
+        
+    }
+}
